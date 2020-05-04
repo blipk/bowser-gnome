9cd0adf9880a7470688050f201e99fa1c443dfd7
wouter bolsterlee <wouter@bolsterl.ee>
2020-05-04 09:59:12 +0200

    normalise whitespace
    Fixed many instances of trailing whitespace and missing newlines at
    the end of files. Depending on the editor, makes editing and producing
    minimal git diffs much easier.

--- bowser-gnome@kronosoul.xyz/fileUtils.js
+++ bowser-gnome@kronosoul.xyz/fileUtils.js
@@ -2,7 +2,7 @@
  * Bowser extension for Gnome 3
  * This file is part of the Bowser Gnome Extension for Gnome 3
  * Copyright (C) 2020 A.D. - http://kronosoul.xyz
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
@@ -15,7 +15,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  */
 
 // External imports
@@ -71,7 +71,7 @@ function enumarateDirectoryChildren(directory=CONF_DIR, returnFiles=true, return
     let directoryFile = Gio.file_new_for_path(directory);
     if (!directoryFile.query_exists(null)) throw Error(directory+' not found');
     let children = directoryFile.enumerate_children('standard::name,standard::type', Gio.FileQueryInfoFlags.NONE, null);
-    
+
     let fileIterator;
     while ((fileIterator = children.next_file(null)) != null) {
         let type = fileIterator.get_file_type();
@@ -82,10 +82,10 @@ function enumarateDirectoryChildren(directory=CONF_DIR, returnFiles=true, return
         let nameWithoutExtension = tmpExtension.join('.');
 
         if (type == Gio.FileType.REGULAR) {
-            if (returnFiles) 
+            if (returnFiles)
                 childrenFilePropertiesArray.push({parentDirectory: directory, fullname: name, name: nameWithoutExtension, extension: extension, type: type});
         } else if (type == Gio.FileType.DIRECTORY) {
-            if (returnDirectories) 
+            if (returnDirectories)
                 childrenFilePropertiesArray.push({parentDirectory: directory, fullname: name, name: nameWithoutExtension, extension: extension, type: type});
             if (!searchSubDirectories) continue;
             let childDirectory = directoryFile.get_child(fileIterator.get_name());
@@ -109,7 +109,7 @@ function saveRawToFile (rawobject, filename, directory=CONF_DIR, append=false, a
     if (async) {
         if (append) {
             file.append_to_async(Gio.FileCreateFlags.NONE, GLib.PRIORITY_DEFAULT, null, function(obj, res) {aSyncSaveCallback(obj, res, contents);});
-        } else {        
+        } else {
             file.replace_async(null, false, Gio.FileCreateFlags.NONE, GLib.PRIORITY_DEFAULT, null, function (obj, res) {aSyncSaveCallback(obj, res, contents);});
         }
     } else {
@@ -117,7 +117,7 @@ function saveRawToFile (rawobject, filename, directory=CONF_DIR, append=false, a
             let outstream = file.append_to(Gio.FileCreateFlags.NONE, null);
             outstream.write(contentsString, null);
             outstream.close(null);
-        } else {        
+        } else {
             let outstream = file.replace(null, false, Gio.FileCreateFlags.NONE, null);
             outstream.write(contentsString, null);
             outstream.close(null);
@@ -136,7 +136,7 @@ function saveJSObjectToFile (jsobject, filename, directory=CONF_DIR, append=fals
     if (async) {
         if (append) {
             file.append_to_async(Gio.FileCreateFlags.NONE, GLib.PRIORITY_DEFAULT, null, function(obj, res) {aSyncSaveCallback(obj, res, contents);});
-        } else {        
+        } else {
             file.replace_async(null, false, Gio.FileCreateFlags.NONE, GLib.PRIORITY_DEFAULT, null, function (obj, res) {aSyncSaveCallback(obj, res, contents);});
         }
     } else {
@@ -155,7 +155,7 @@ function saveJSObjectToFile (jsobject, filename, directory=CONF_DIR, append=fals
 }
 function aSyncSaveCallback(obj, res, contents) {
     let stream = obj.replace_finish(res);
-    
+
     stream.write_bytes_async(contents, GLib.PRIORITY_DEFAULT, null, function (w_obj, w_res) {
         w_obj.write_bytes_finish(w_res); stream.close(null);
     });
@@ -170,9 +170,9 @@ function loadJSObjectFromFile(filename=BOWSER_CONF_FILE, directory=CONF_DIR, cal
     if (!GLib.file_test(loadPath, GLib.FileTest.EXISTS)) { throw Error("File does not exist: "+loadPath); }
     if (async === true) {
         if (typeof callback !== 'function') {throw TypeError('loadJSObjectFromFile callback must be a function');}
-        
+
         file.query_info_async('*', Gio.FileQueryInfoFlags.NONE, GLib.PRIORITY_DEFAULT, null, function (src, res) {
-            let file_info = src.query_info_finish(res);    
+            let file_info = src.query_info_finish(res);
             file.load_contents_async(null, function (obj, res) {
                 let [success, contents] = obj.load_contents_finish(res);
                 if (success) {
@@ -181,7 +181,7 @@ function loadJSObjectFromFile(filename=BOWSER_CONF_FILE, directory=CONF_DIR, cal
                     callback(jsobject);
                 }
             });
-        });           
+        });
     } else {
         //let buffer = file.load_contents(null, null, null);
         let buffer = file.load_contents(null);
