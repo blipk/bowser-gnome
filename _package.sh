5a8f6d891cad0b4f725aedb498e088006a8c8ecf
kronosoul <>
2020-05-05 12:30:20 +1000

    update installer

--- _package.sh
+++ _package.sh
@@ -2,3 +2,4 @@
 glib-compile-resources ./res/org.gnome.shell.extensions.bowser-gnome.xml
 mv ./res/org.gnome.shell.extensions.bowser-gnome.gresource ./bowser-gnome@kronosoul.xyz
 zip -jr bowser-gnome@kronosoul.xyz.zip bowser-gnome@kronosoul.xyz
+zip bowser-gnome@kronosoul.xyz.zip install.sh 'Install Bowser Gnome.desktop'
